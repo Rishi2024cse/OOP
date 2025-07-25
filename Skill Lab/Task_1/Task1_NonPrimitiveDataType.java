7c6de7d109cf3f085de7e851a3c56706c72d61eb
Rishi2024cse <2420030638@klh.edu.in>
2025-07-25 18:11:55 +0530

    Delete Skill Lab/Task_1/Task1_NonPrimitiveDataType.java

--- Skill Lab/Task_1/Task1_NonPrimitiveDataType.java
+++
@@ -1,9 +0,0 @@
-package practical;
-
-public class Task1_NonPrimitiveDataType {
-    public static void main(String[] args) {
-        String greeting = "Hello, World!";
-        System.out.println(greeting);
-    }
-
-}
