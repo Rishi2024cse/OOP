dbfc4feb2437c7c1660ffb9dfc327d4323d9d809
Rishi2024cse <2420030638@klh.edu.in>
2025-07-25 13:23:55 +0530

    Add files via upload

---
+++ Practical Lab/Task_1/Task1_Operators.java
@@ -0,0 +1,38 @@
+package practical;
+
+public class Task1_Operators {
+	public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        // Arithmetic operators
+        int sum = a + b;          
+        int diff = a - b;         
+        int product = a * b;      
+        int quotient = a / b;     
+        int remainder = a % b; 
+        System.out.println("Arithmetic Operators:");
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+        // Relational operators
+        System.out.println("\nRelational Operators:");
+        System.out.println("a == b: " + (a == b));   
+        System.out.println("a != b: " + (a != b));   
+        System.out.println("a > b: " + (a > b));     
+        System.out.println("a < b: " + (a < b));     
+        System.out.println("a >= b: " + (a >= b));   
+        System.out.println("a <= b: " + (a <= b));   
+        // Logical operators
+        boolean x = true;
+        boolean y = false;
+        System.out.println("\nLogical Operators:");
+        System.out.println("x && y: " + (x && y));   
+        System.out.println("x || y: " + (x || y));  
+        System.out.println("!x: " + (!x));           
+    }
+
+}
+
+
