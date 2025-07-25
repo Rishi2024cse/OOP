f2d92c1fbe784d4be1282798c0fbf085263b66b2
Rishi2024cse <2420030638@klh.edu.in>
2025-07-25 17:57:56 +0530

    Add files via upload

---
+++ Skill Lab/Task_1/Task1_WrapperClasses1.java
@@ -0,0 +1,13 @@
+package skill;
+
+public class Task1_WrapperClasses1 {
+	public static void main(String args[])
+	{  
+	    //Converting int into Integer  
+	    int a=20;  
+	   Integer i=Integer.valueOf(a);
+	   Integer j=a;  
+	   System.out.println(a+" "+i+" "+j);  
+	  }
+
+}
---
+++ Skill Lab/Task_1/Task1_WrapperClasses2.java
@@ -0,0 +1,12 @@
+package skill;
+
+public class Task1_WrapperClasses2 {
+    public static void main(String args[]) {
+        // Converting Integer to int
+        Integer a = 3;
+        int i = a.intValue();       
+        int j = a;                  
+
+        System.out.println(a + " " + i + " " + j);
+    }
+}
