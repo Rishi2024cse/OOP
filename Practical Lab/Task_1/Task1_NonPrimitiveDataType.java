9d6e0fce15808cc051478361c48d4559a4af7d66
Rishi2024cse <2420030638@klh.edu.in>
2025-07-25 18:12:42 +0530

    Add files via upload

---
+++ Practical Lab/Task_1/Task1_NonPrimitiveDataType.java
@@ -0,0 +1,9 @@
+package practical;
+
+public class Task1_NonPrimitiveDataType {
+    public static void main(String[] args) {
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+    }
+
+}
