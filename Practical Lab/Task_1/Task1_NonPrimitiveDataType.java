607eb52caa0f453aa182b8e611f547dad08b7fd0
Rishi2024cse <2420030638@klh.edu.in>
2025-07-26 11:27:04 +0530

    Update Task1_NonPrimitiveDataType.java

--- Practical Lab/Task_1/Task1_NonPrimitiveDataType.java
+++ Practical Lab/Task_1/Task1_NonPrimitiveDataType.java
@@ -2,7 +2,7 @@ package practical;
 
 public class Task1_NonPrimitiveDataType {
     public static void main(String[] args) {
-        String greeting = "Hello";
+        String greeting = "Hello Java";
         System.out.println(greeting);
     }
 
